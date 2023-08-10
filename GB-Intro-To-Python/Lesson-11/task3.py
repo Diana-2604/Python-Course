61599f872369c0491c7714e03815d15337731fc8
Diana Kirillova <66692981+Diana-2604@users.noreply.github.com>
2023-08-10 22:11:02 +0300

    Add files via upload

---
+++ GB-Intro-To-Python/Lesson-11/task3.py
@@ -0,0 +1,37 @@
+# Создайте класс Матрица. Добавьте методы для:
+# ○ вывода на печать,
+# ○ сравнения,
+# ○ сложения,
+# ○ *умножения матриц
+
+
+class Matrix:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.data = [[0] * cols for _ in range(rows)]
+
+    def __str__(self):
+        return ' '.join([' '.join(map(str, row)) for row in self.data])
+
+    def __eq__(self, other):
+        return self.data == other.data
+
+    def __add__(self, other):
+        if self.rows != other.rows or self.cols != other.cols:
+            raise ValueError("Matrices must have the same dimensions for addition.")
+        result = Matrix(self.rows, self.cols)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result.data[i][j] = self.data[i][j] + other.data[i][j]
+        return result
+
+    def __mul__(self, other):
+        if self.cols != other.rows:
+            raise ValueError("Number of columns in the first matrix must match the number of rows in the second matrix for multiplication.")
+        result = Matrix(self.rows, other.cols)
+        for i in range(self.rows):
+            for j in range(other.cols):
+                for k in range(self.cols):
+                    result.data[i][j] += self.data[i][k] * other.data[k][j]
+        return result
